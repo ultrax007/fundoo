c15b6e7347786cca12b0a5cfbb3c2080900f9a98
ultrax007 <rajat.sawarkar@gmail.com>
2019-11-22 23:24:58 +0530

    done 4 pages for now

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import "./App.css";
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<Switch>
 					<Route path="/" exact component={Login}></Route>
 					<Route path="/register" component={Register}></Route>
+					<Route path="/forgotPassword" component={ForgotPassword}></Route>
 				</Switch>
 			</BrowserRouter>
 		</div>
