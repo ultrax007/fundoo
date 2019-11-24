0df2b703b49f583ad5f6e6adf67d3f36c5512e0c
ultrax007 <rajat.sawarkar@gmail.com>
2019-11-25 00:53:57 +0530

    done appbar

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,64 +1,99 @@
 import React from "react";
-import "../sass/dashboard.sass"
-// import { fade } from "@material-ui/core/styles";
+import "../sass/dashboard.sass";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-// import Typography from "@material-ui/core/Typography";
-// import InputBase from "@material-ui/core/InputBase";
-// import Badge from "@material-ui/core/Badge";
-// import MenuItem from "@material-ui/core/MenuItem";
-// import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
-// import SearchIcon from "@material-ui/icons/Search";
-// import AccountCircle from "@material-ui/icons/AccountCircle";
-// import MailIcon from "@material-ui/icons/Mail";
-// import NotificationsIcon from "@material-ui/icons/Notifications";
-// import MoreIcon from "@material-ui/icons/MoreVert";
+import flogo from "../assets/favicon.ico";
+import Box from "@material-ui/core/Box";
+import SearchIcon from "@material-ui/icons/Search";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import ViewAgendaOutlinedIcon from "@material-ui/icons/ViewAgendaOutlined";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import Drawer from '@material-ui/core/Drawer';
 const theme = createMuiTheme({
   overrides: {
     MuiAppBar: {
       colorPrimary: {
-        color:"#424242",
-        backgroundColor: "white"        
+        color: "#424242",
+        backgroundColor: "white"
+      }
+    },
+    MuiFormControl: {
+      root: {
+        minWidth: "60%"
       }
     }
-	}
+  }
 });
 export default class Dashboard extends React.Component {
-	render() {
-		const classes = {
-			root: {
-				flexGrow: 1
-      },
-      menuButton: {
-        marginRight: theme.spacing(2),
-      },
-		};
+  render() {
+    // const classes = {};
 
-		return (
-			<div className="main">
-				<MuiThemeProvider theme={theme}>
-					<div className="header">
-						<AppBar position="static">
-							<Toolbar>
-								<IconButton
-									edge="start"
-									style={classes.menuButton}
-									color="inherit"
-									aria-label="open drawer"
-								>
-									<MenuIcon />
-								</IconButton>
-							</Toolbar>
-						</AppBar>
+    return (
+      <div className="main">
+        <MuiThemeProvider theme={theme}>
+          <div className="header">
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <div id="logo">
+                  <div className="flogo">
+                    <img
+                      src={flogo}
+                      alt="Fundoo"
+                      height="90%"
+                      width="80%"
+                    ></img>
+                  </div>
+                  <Box component="span" className="flogotext">
+                    Fundoo
+                  </Box>
+                </div>
+                <div className="searchBlock">
+                  <TextField
+                    id="searchtext"
+                    type="text"
+                    margin="dense"
+                    width="50%"
+                    height="90%"
+                    placeholder="search"
+                    // variant="outlined"
+                    // style={classes.search}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SearchIcon />
+                        </InputAdornment>
+                      )
+                    }}
+                  />
+                </div>
+                <div id="rightPanelLogo">
+                  <ViewAgendaOutlinedIcon />
+                  <ShoppingCartOutlinedIcon />
+                </div>
+                <div id="avatar">
+                  <Avatar>
+                    <AccountCircleOutlinedIcon/>
+                  </Avatar>
+                </div>
+              </Toolbar>
+            </AppBar>
           </div>
-          <div className="container">
-
-          </div>
-				</MuiThemeProvider>
-			</div>
-		);
-	}
+          <div className="container"></div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
